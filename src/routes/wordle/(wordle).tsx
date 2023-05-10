60a176534aba8d12a74827222253cdd2afc7c206
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2023-05-10 13:57:49 +0200

    wordle

---
+++ src/routes/wordle/(wordle).tsx
@@ -0,0 +1,10 @@
+import Wordle from "~/components/Wordle/Wordle";
+import Page from "~/components/shared/Page";
+
+export default function WordlePage() {
+    return (
+        <Page title="Wordle">
+            <Wordle />
+        </Page>
+    )
+}
