b57383a18e945fb417ab1ac2eabe7445466e0d70
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2023-07-21 10:49:39 +0200

    ag grid

--- src/routes/admin/games/(games).tsx
+++ src/routes/admin/games/(games).tsx
@@ -1,12 +1,11 @@
 import { type Resource, Suspense, Show, createSignal, createEffect } from "solid-js";
-import { unstable_clientOnly, useRouteData } from "solid-start";
+import { A, unstable_clientOnly, useRouteData } from "solid-start";
 import { db } from "~/db";
-import { developer, game, publisher } from "~/drizzle/schema";
+import { developer, game, genresOfGames, publisher } from "~/drizzle/schema";
 import styles from "../admin.module.scss"
 import { eq } from "drizzle-orm"
 import { createServerData$ } from "solid-start/server";
-import { type SortingState, type PaginationState } from "@tanstack/solid-table";
-import { ColDef, GridOptions, ICellEditor, ICellEditorParams } from "ag-grid-community";
+import { ColDef, GridOptions, ICellEditor, ICellEditorParams, ICellRendererParams } from "ag-grid-community";
 import 'ag-grid-community/styles/ag-grid.css'; // grid core CSS
 import "ag-grid-community/styles/ag-theme-alpine.css"; // optional theme
 import { AgGridSolidRef } from "ag-grid-solid";
@@ -14,12 +13,16 @@ import { ChangeEvent } from "~/lib/solidTypes";
 
 const AgGridSolid = unstable_clientOnly(() => import("ag-grid-solid"));
 export function routeData() {
-    return createServerData$(async () => db
-        .select()
-        .from(game)
-        .innerJoin(developer, eq(game.developerId, developer.developerId))
-        .innerJoin(publisher, eq(game.publisherId, publisher.publisherId))
-    )
+    return createServerData$(async () => {
+        const result = await db
+            .select()
+            .from(game)
+            .innerJoin(developer, eq(game.developerId, developer.developerId))
+            .innerJoin(publisher, eq(game.publisherId, publisher.publisherId))
+            .innerJoin(genresOfGames, eq(game.gameId, genresOfGames.gameId))
+            .orderBy(game.gameId)
+        
+    })
 }
 
 type UnwrapResource<T> = T extends Resource<infer x | undefined> ? x : never
@@ -37,15 +40,35 @@ const columnDefs: Cols[] = [{
     cellEditor: DataEditor,
 }, {
     field: 'Developer.name',
+    filter: true
 }, {
     field: 'Publisher.name',
+    filter: true
 }, {
     field: 'Game.releaseDate',
+}, {
+    headerName: 'Testing',
+    cellRenderer: (params: ICellRendererParams) =>
+        <AdminLink
+            {...params}
+        />,
+    cellRendererParams: {
+        value: "dkfsjlkd"
+    },
+}, {
+    field: 'GenresOfGames.genre'
 }]
+
+function AdminLink(props: ICellRendererParams) {
+    // console.log(unwrap(props))
+    return (
+        <A href={"/tes"}>Edit {props.value}</A>
+    )
+}
 
 export default function GamesAdminPage() {
     const data = useRouteData<typeof routeData>()
-    
+
     return (
         <main class={`${styles.main} ag-theme-alpine-dark`} style={{ width: '100%', height: '100vh' }}>
             <Suspense fallback={<span>loading...</span>}>
