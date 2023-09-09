a68df18a7f6702b1026643ef126ec6fe20c1bc17
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2023-09-09 21:43:52 +0200

    test deploy

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,9 @@
-import { sql } from '@vercel/postgres';
+import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from "~/drizzle/schema"
-import { drizzle } from 'drizzle-orm/vercel-postgres';
 
-const client = postgres(process.env.POSTGRES_URL!);
-export const db = drizzle(sql, {schema});
+const client = postgres(process.env.POSTGRES_URL!, {
+    ssl: true
+});
+export const db = drizzle(client, {schema});
  
