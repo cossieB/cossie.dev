7aaf5f59ee029eedaa7d7e9e70028f12717e8586
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2023-10-03 19:14:23 +0200

    updated actor creation form

--- src/components/admin/actors/ActorForm.tsx
+++ src/components/admin/actors/ActorForm.tsx
@@ -2,7 +2,7 @@ import { Actor, Game } from "~/drizzle/types";
 import AdminForm from "../AdminForm";
 import styles from "~/components/admin/forms/forms.module.scss";
 import { SetStoreFunction, createStore } from "solid-js/store";
-import { For, createEffect, createMemo, createSignal, useContext } from "solid-js";
+import { For, Show, createEffect, createMemo, createSignal, useContext } from "solid-js";
 import { createServerAction$ } from "solid-start/server";
 import { updateActorOnDB } from "./updateActorOnDB";
 import { FormInput } from "../forms/FormInput"
@@ -10,14 +10,17 @@ import CustomTextarea from "../CustomTextarea";
 import { DropZone } from "../forms/DropZone";
 import HiddenInput from "../forms/HiddenInput";
 import { AdminContext } from "~/routes/admin";
-
+import { ChevronDown } from "~/svgs";
+import clickOutside from "~/lib/clickOutside";
+false && clickOutside
 type Props = {
     data?: Actor
 }
 type NewType = {
     gameId: string;
-    title: string;
-    charName: string;
+    gameTitle: string;
+    character: string;
+    importance: number
 };
 function copyData(data: Props['data']): Actor {
     return {
@@ -59,7 +62,7 @@ export default function ActorForm(props: Props) {
             setState={setState}
             submitDisabled={
                 !actor.name ||
-                characters.list.some(x => x.charName === "")
+                characters.list.some(x => !x.character)
             }
         >
             <FormInput
@@ -84,24 +87,53 @@ export default function ActorForm(props: Props) {
                 name="summary"
                 value={actor.summary ?? ""}
             />
-            <div style={{ display: 'flex', position: 'relative' }}>
-                <button onclick={() => setState('modalOpen', prev => !prev)} type="button">
-                    Add Character
+            <div class={styles.btnDiv}>
+                <button
+                    onclick={() => setState('modalOpen', prev => !prev)}
+                    type="button"
+                    classList={{ [styles.open]: state.modalOpen }}
+                >
+                    Add Character{" "}
+                    <ChevronDown />
                 </button>
                 <GameSelector
                     setCharacters={setCharacters}
                     characters={characters}
                     modalOpen={state.modalOpen}
+                    closeModal={() => setState('modalOpen', false)}
                 />
             </div>
             <For each={characters.list}>
                 {char =>
                     <li class={styles.charList}>
-                        <span>{char.title}</span>
-                        <input type="text" required placeholder={`Character in ${char.title}`} />
+                        <span>{char.gameTitle}</span>
+                        <input
+                            type="text" required
+                            placeholder={`Character in ${char.gameTitle}`}
+                            oninput={e => {
+                                setCharacters('list', characters.list.findIndex(x => x.gameId === char.gameId), 'character', e.target.value)
+                            }}
+                            value={char.character}
+                        />
+                        <select 
+                            onchange={e => {
+                                setCharacters('list', characters.list.findIndex(x => x.gameId === char.gameId), 'importance', Number(e.target.value))
+                            }}
+                        >
+                            <option disabled>Role Type</option>
+                            <option selected={characters.list.find(x => x.gameId === char.gameId)?.importance == 1} value="1">Player Character</option>
+                            <option selected={characters.list.find(x => x.gameId === char.gameId)?.importance == 2} value="2">Main Role</option>
+                            <option selected={characters.list.find(x => x.gameId === char.gameId)?.importance == 3} value="3">Supporting Character</option>
+                            <option selected={characters.list.find(x => x.gameId === char.gameId)?.importance == 4} value="4">Background Character</option>
+                            <option selected={characters.list.find(x => x.gameId === char.gameId)?.importance == 5} value="5">Extra</option>
+                        </select>
                     </li>
                 }
             </For>
+            <Show when={actor.photo}>
+                <HiddenInput name="photo" value={actor.photo!} />
+            </Show>
+            <HiddenInput name="characters" value={JSON.stringify(characters.list)} />
             <HiddenInput name="newItem" value={props.data ? 0 : 1} />
         </AdminForm>
     )
@@ -116,15 +148,20 @@ type P = {
         list: NewType[];
         readonly set: Set<string>;
     }>
-    modalOpen: boolean
+    modalOpen: boolean,
+    closeModal(): void
 }
 
 function GameSelector(props: P) {
     const { games } = useContext(AdminContext)!
-    const [input, setInput] = createSignal("w")
+    const [input, setInput] = createSignal("")
     const filtered = createMemo(() => games.filter(game => game.title.toLowerCase().includes(input().toLowerCase())))
     return (
-        <ul class={styles.selector} classList={{[styles.open]: props.modalOpen}}>
+        <ul
+            class={styles.selector}
+            classList={{ [styles.open]: props.modalOpen }}
+            use:clickOutside={props.closeModal}
+        >
             <input type="search"
                 value={input()}
                 onInput={e => setInput(e.target.value)}
@@ -153,8 +190,9 @@ function Li(props: P & { game: Game }) {
             props.setCharacters(prev => ({
                 list: [...prev.list, {
                     gameId: props.game.gameId,
-                    title: props.game.title,
-                    charName: ""
+                    gameTitle: props.game.title,
+                    character: "",
+                    importance: 2
                 }]
             }))
     }
--- src/components/admin/actors/updateActorOnDB.tsx
+++ src/components/admin/actors/updateActorOnDB.tsx
@@ -1,13 +1,20 @@
 import {ServerError, type ServerFunctionEvent } from "solid-start";
 import { authenticateOrThrowUnauthorized } from "~/utils/authenticate";
 
-export async function updateActorOnDB(fd: FormData, event: ServerFunctionEvent): Promise<void> {
-    await authenticateOrThrowUnauthorized(event.request);
+export async function updateActorOnDB(fd: FormData, event: ServerFunctionEvent) {
+    // await authenticateOrThrowUnauthorized(event.request);
     const obj: { [key: string]: string; } = {};
+    let characters: {gameId: string, character: string, importance: string}[] = []
     fd.forEach((val, key) => {
         if (typeof val != "string")
             throw new ServerError('Invalid Format', { status: 400 })
-        obj[key] = val;
+        else if (key == 'characters') {
+            characters = JSON.parse(val)
+        }
+        else {
+            obj[key] = val;
+        }
     })
-    const {actorId, newItem, ...a} = obj
+console.log(characters)
+    return 'ok'
 }
