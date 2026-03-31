7d9f445cd5d26e56b58075fc89b64e9cacc9d04f
Buntu Cossie <91845390+cossieB@users.noreply.github.com>
2026-03-31 16:14:42 +0200

    start solidstart 2 migration

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { solidStart } from "@solidjs/start/config";
+import { defineConfig } from "vite";
+import { nitroV2Plugin } from "@solidjs/vite-plugin-nitro-2";
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      solidStart(),
+      nitroV2Plugin({
+        preset: "vercel",
+        prerender: {
+            crawlLinks: true,
+        }
+      }),
+    ],
+  };
+});
